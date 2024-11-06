9ac6cd744fb3d5ad76fd6a77a01cbcccaade8752
Talha <talhahaider074@gmail.com>
2024-11-06 20:42:48 +0500

    admin account api completed

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const combineRouter = require("express").Router()
 const physicanAccount = require("./physicans/account.router")
 const cTemsAccount = require("./ctems/account.router")
+const adminAccount = require("./admin/account.router")
+
 
 
 combineRouter.use("/physician",physicanAccount)
 combineRouter.use("/ctems",cTemsAccount)
+combineRouter.use("/admin",adminAccount)
 
 
 
